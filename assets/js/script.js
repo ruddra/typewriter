dd93b961615ff67214a1d3db9a5225bebcef590d
Ahmad Ajmi <ahmad@ahmadajmi.com>
2016-06-22 18:47:25 +0200

    Update gulpfile & remove un-used npm packages & clean scrip.js for un-used code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,6 @@
-'use strict';
-
 $(document).ready(function() {
-  $(".wrapper").fitVids();
 
-  // Add class current to nav item
-  // http://stackoverflow.com/a/33490386/558777
-  $('.site-nav a').each(function() {
-    if($(this).attr('href') == window.location.href) {
-      $(this).addClass('current');
-    }
-  });
+  'use strict';
+
+  $('.wrapper').fitVids();
 });
--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,48 @@
-'use strict';
+(function() {
 
-// Include Gulp & tools
+  'use strict';
 
-var gulp         = require('gulp');
-var watch        = require('gulp-watch');
-var sass         = require('gulp-sass');
-var minifycss    = require('gulp-minify-css');
-var autoprefixer = require('gulp-autoprefixer');
-var runSequence  = require('run-sequence');
-var concat       = require('gulp-concat');
-var rename       = require('gulp-rename');
-var plumber      = require('gulp-plumber');
-var gutil        = require('gulp-util');
+  // Include Gulp & tools
+  var gulp = require('gulp'),
+      sass         = require('gulp-sass'),
+      minifycss    = require('gulp-minify-css'),
+      autoprefixer = require('gulp-autoprefixer'),
+      runSequence  = require('run-sequence'),
+      rename       = require('gulp-rename'),
+      plumber      = require('gulp-plumber'),
+      gutil        = require('gulp-util');
 
-var onError = function( err ) {
-  console.log('An error occurred:', gutil.colors.magenta(err.message));
-  gutil.beep();
-  this.emit('end');
-}
+  var onError = function( err ) {
+    console.log('An error occurred:', gutil.colors.magenta(err.message));
+    gutil.beep();
+    this.emit('end');
+  };
 
-// SASS
+  // SASS
+  gulp.task('sass', function () {
+    return gulp.src('./assets/sass/*.scss')
+    .pipe(plumber({ errorHandler: onError }))
+    .pipe(sass())
+    .pipe(autoprefixer())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./assets/css'));
+  });
 
-gulp.task('sass', function () {
-  return gulp.src('./assets/sass/*.scss')
-  .pipe(plumber({ errorHandler: onError }))
-  .pipe(sass())
-  .pipe(autoprefixer())
-  .pipe(rename({suffix: '.min'}))
-  .pipe(minifycss())
-  .pipe(gulp.dest('./assets/css'));
-});
-
-// Watch
-
-gulp.task('watch', function() {
-  gulp.watch('./assets/sass/**/*.scss', ['sass']);
-});
+  // Watch
+  gulp.task('watch', function() {
+    gulp.watch('./assets/sass/**/*.scss', ['sass']);
+  });
 
 
-// Build
+  // Build
+  gulp.task('build', [], function() {
+    runSequence('sass');
+  });
 
-gulp.task('build', [], function() {
-  runSequence('sass');
-});
+  // Default
+  gulp.task('default', ['watch'], function() {
+    gulp.start('build');
+  });
 
-// Gulp Default
-
-gulp.task('default', ['watch'], function() {
-  gulp.start('build');
-});
+})();
